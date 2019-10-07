6cd2dbfbba2d4b6890bd6cabe1b23cdc37d4a5ed
Huactemo Cahue <temocahue@yahoo.com>
2019-10-07 12:17:47 -0500

    class done

--- js/app.js
+++ js/app.js
@@ -1,2 +1,50 @@
+class Card {
 
-   
+	constructor(text) {
+		this.text = text 
+	}
+
+	display() {
+		// 2. show card (back) to the user using jquery
+		const card1 = new Card(emoji[0])
+		
+	}
+
+	turn() {
+		// 3. visual -- see card face instead card back
+
+		// 4. make it flip back either after some time or after they click again
+	}
+
+	compare(anotherCard) {
+		// 1. compare the two cards
+
+		// boolean -- true or false
+		// print true or false
+
+	}
+
+}
+
+
+
+
+const emoji = ['🥞', '🥞', '🍰', '🍰', '🍪', '🍪', '🍡', '🍡', '🍮', '🍮', '🍦', '🍦'];
+
+
+
+
+
+const game = {
+
+}
+
+
+
+// event listeners -- part of step 3
+
+
+
+
+
+
