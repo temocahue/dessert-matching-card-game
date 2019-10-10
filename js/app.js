c53c17df4e0d8626576a140a6b3cbff5f0819436
Huactemo Cahue <temocahue@yahoo.com>
2019-10-10 14:29:16 -0500

    cards opacity changes in game play

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ const emoji = ['🥞', '🥞', '🍰', '🍰', '🍪', '🍪', '🍡', '🍡', '
 
 const game = {
 	timePassed:100, 
-    points:0,
+    matchPoints:0,
 
     //store element
     cardClicked: null,
@@ -22,7 +22,7 @@ const game = {
 	display() {
 		
 		while(emoji.length > 0) {
-			
+			console.log(this.cardClicked);
 			// get a rand index number so we can get one emoji
 			const randIndex = Math.floor(Math.random() * emoji.length)
 
@@ -63,26 +63,39 @@ const game = {
 	// },
 
 	turn(card) {
-		console.log("here's the card element");
-		console.log(card);
+		const $card = $(card)
+		const currentOpacity = $(card).css('opacity')
+		if(currentOpacity == 1) {
+			$(card).css('opacity', 0)
+		} else if(currentOpacity == 0){
+			$(card).css('opacity', 1)
+		}
+		// const getCard = $(card)[0].className
+		// console.log("this is the card jquery object");
+		// console.log($card[0].className);
+		// console.log(this.cardClicked);
+
+		// if no card was click previously (else)
+		if(this.cardClicked === null) {
+			// let game know the element that was clicked -- store in this.cardClicked
+			// so any card after the first click will be compared to the previous card.
+			this.cardClicked = $card
+		
+		} else if (this.cardClicked !== null){
+			
+			//new function where you pass in  and $card
+			this.compareCards($card)
+			
+
+
+
+		}
+
 
 		// if previous card was clicked -- this.cardClicked not null
 			
-			// compare:
-			// if the previous card matched the first card that was clicked 
-				// (keep cards face up)
-				// add point to cards matched
 
-			/// if previous card does not match the card that was clicked 
-				// both cards face down
-				// (dont add points)
-
-
-		// if no card was click previously (else)
 			// (dont compare)
-			// let game know the element that was clicked -- store in this.cardClicked
-			// so any card after the first click will be compared to the previous card.
-		
 
 
 
@@ -93,31 +106,88 @@ const game = {
 
 
 
-		const currentOpacity = $(card).css('opacity')
-		if(currentOpacity == 1) {
-			$(card).css('opacity', 0)
-		} else if(currentOpacity == 0){
-			$(card).css('opacity', 1)
+		
+	},
+	
+
+	
+	compareCards($card){
+
+		const $card3 = $(this.cardClicked)
+		const $card2 = $card3[0].className
+		const $card1 = $card[0].className
+       
+		
+		
+		// get the val
+
+
+
+		// if the previous card matched the first card that was clicked 
+		if($card1 === $card2){
+		
+			console.log(true);
+			this.matchPoints +=1
+
+			setTimeout(() => {
+		    $('p').hide()
+
+
+
+			}, 500)
+
+			// setTimeout
+				// (keep cards face up -- remove emoji?)
+			
+				// add point to cards matched
+    	
+
+		
+		/// if previous card does not match the card that was clicked 
+		} else {
+			setTimeout(() => {
+				$card.css('opacity', 1)
+				$card3.css('opacity', 1)
+
+
+
+
+
+			}, 500)
+			
+			console.log(false);
+
+
+			// setTimeout
+				// both cards face down currentOpacity = 1
+
+				// (dont add points)
 		}
 
+
+
+
+	},
+	
+	endturn(){
+	  	//user shouldnt be able to click the card thats flipped face up
+       	// end turn if match of two is made and true,
+       	// end turn after two cards are clicked and match is false, face down
+       	// only two cards at a time .. after 2 card clicks disable listiner
+       //
+
+
+
 	}
-	
 
-	
-	
-	// compare(card1, card2) {
-	
-	// 	if(card1.text === card2.text){
-	//    		console.log(true); 
-	// 	}
+ }
+
+
 		
-		
-
-	// }
+ 
 
 	
 
-}
 
 
 
